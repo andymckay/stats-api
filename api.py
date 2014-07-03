963b8f477273ea6848d23014a9ae6ace4af5a437
Andy McKay <amckay@mozilla.com>
2014-07-03 14:30:46 -0700

    add in apps_added_by_package

--- api.py
+++ api.py
@@ -1,25 +1,43 @@
 import pprint
 import os
+import urllib
 
 import marketplace
 
 
 marketplace.client.URLS['settings.mine'] = '/account/settings/mine/'
+marketplace.client.URLS['stats.apps_added_by_package'] = '/stats/global/apps_added_by_package/?%s'
 
 
-def settings():
-    for setting in ['CONSUMER_KEY', 'CONSUMER_SECRET']:
-        if not os.environ.get(setting):
-            raise ValueError('%s is not set in the environment' % setting)
+class Client(object):
 
-    client = marketplace.Client(domain='marketplace.firefox.com',
-                                protocol='https',
-                                consumer_key=os.environ['CONSUMER_KEY'],
-                                consumer_secret=os.environ['CONSUMER_SECRET'])
-    result = client.conn.fetch('GET', client.url('settings.mine'))
-    print result.status_code
+    def __init__(self):
+        for setting in ['CONSUMER_KEY', 'CONSUMER_SECRET']:
+            if not os.environ.get(setting):
+                raise ValueError('%s is not set in the environment' % setting)
+
+        self.client = marketplace.Client(domain='marketplace.firefox.com',
+            protocol='https',
+            consumer_key=os.environ['CONSUMER_KEY'],
+            consumer_secret=os.environ['CONSUMER_SECRET'])
+
+
+    def get(self, url, data=None):
+        target = self.client.url(url)
+        if data:
+            data = urllib.urlencode(data)
+            target = target % data
+        return self.client.conn.fetch('GET', target)
+
+
+def dump(result):
     pprint.pprint(result.json())
 
 
 if __name__ == '__main__':
-    settings()
+    c = Client()
+    dump(c.get('settings.mine'))
+    dump(c.get('stats.apps_added_by_package', {
+        'start': '2014-01-01',
+        'end': '2014-01-03',
+        'interval': 'day'}))
